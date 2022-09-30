56ce0edded437bf928b94bad5a288be51e489d6b
pedroajs <pedroajs93@gmail.com>
2022-09-30 15:34:32 -0300

    Teste do método charge

---
+++ solutions/devsprint-luiz-zytkowski-4/src/test/io/devpass/creditcard/transport/controllers/CreditCardOperationControllerTest.kt
@@ -0,0 +1,52 @@
+package io.devpass.creditcard.transport.controllers
+
+import io.devpass.creditcard.domain.objects.CreditCardOperation
+import io.devpass.creditcard.domainaccess.ICreditCardOperationServiceAdapter
+import io.devpass.creditcard.transport.requests.CreditCardChargeRequest
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+class CreditCardOperationControllerTest {
+
+
+    //TESTAR O METODO CHARGE
+    // O METODO CHARGE BATE NO SERIVCE E RETORNA UMA LISTA DE CREDITCARD OP
+
+    @Test
+    fun `should return a list of credit card operations`(){
+        //mockar o adapter
+        val request = CreditCardChargeRequest(
+            creditCardId = "",
+            value = 0.0,
+            installments = 1,
+            description = "",
+
+        )
+        val creditCardOperation = getRandomCreditCardOperation()
+        val creditCardOperationService = mockk<ICreditCardOperationServiceAdapter>{
+            every { charge(any()) } returns listOf(creditCardOperation)
+        }
+
+        val creditCardOperationController = CreditCardOperationController(creditCardOperationService)
+
+        val result = creditCardOperationController.charge(request)
+
+        Assertions.assertEquals(listOf(creditCardOperation), result)
+    }
+
+    private fun getRandomCreditCardOperation() : CreditCardOperation{
+        return CreditCardOperation(
+            id = "",
+            creditCard = "",
+            type = "",
+            value = 0.0,
+            month = 1,
+            year = 2022,
+            description = "",
+            createdAt = LocalDateTime.now(),
+        )
+    }
+}
