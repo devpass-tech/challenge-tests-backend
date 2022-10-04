ad2d16e6af6fe419be11637a4d2a18b864c000e5
livioqueiroz <queiroz.livio@gmail.com>
2022-10-04 18:55:56 -0300

    criado testes de cenários para validação de CPF

--- solutions/devsprint-luiz-zytkowski-4/src/test/io/devpass/creditcard/domain/CreditCardServiceTest.kt
+++ solutions/devsprint-luiz-zytkowski-4/src/test/io/devpass/creditcard/domain/CreditCardServiceTest.kt
@@ -4,14 +4,14 @@ package io.devpass.creditcard.domain
 import io.devpass.creditcard.dataaccess.IAccountManagementGateway
 import io.devpass.creditcard.dataaccess.IAntiFraudGateway
 import io.devpass.creditcard.dataaccess.ICreditCardDAO
-import io.devpass.creditcard.domain.exceptions.EntityNotFoundException
+import io.devpass.creditcard.domain.exceptions.BusinessRuleException
 import io.devpass.creditcard.domain.objects.CreditCard
-import io.devpass.creditcard.domainaccess.ICreditCardServiceAdapter
-import io.devpass.creditcard.transport.controllers.CreditCardController
+import io.devpass.creditcard.domain.objects.CreditCardCreation
 import io.mockk.every
 import io.mockk.mockk
 import org.hibernate.TransactionException
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -28,7 +28,7 @@ class CreditCardServiceTest {
         }
         val creditCardService = CreditCardService(creditCardDAO, antiFraudGateway, accountManagementGateway)
         val result = creditCardService.findCreditCardById("")
-        Assertions.assertEquals(creditCardReference, result)
+        assertEquals(creditCardReference, result)
     }
 
 
@@ -43,6 +43,54 @@ class CreditCardServiceTest {
         assertThrows<TransactionException> {
             creditCardService.findCreditCardById("")
         }
+    }
+
+    @Test
+    fun `Should validate CPF sucessfully`() {
+        val antiFraudGateway = mockk<IAntiFraudGateway>()
+        val accountManagementGateway = mockk<IAccountManagementGateway>()
+        val creditCardDAO = mockk<ICreditCardDAO>()
+        val creditCardService = CreditCardService(creditCardDAO, antiFraudGateway, accountManagementGateway)
+
+        val validTaxId = "00761779477"
+        val printedName = "Livio"
+        val creditCardCreation = CreditCardCreation(validTaxId, printedName)
+
+        val exception = assertThrows<Exception> {
+            creditCardService.requestCreation(creditCardCreation)
+        }
+        assertNotEquals(BusinessRuleException("Provided CPF is invalid.").message, exception.message)
+
+    }
+
+    @Test
+    fun `Should throw an Exception when validate CPF`() {
+        val antiFraudGateway = mockk<IAntiFraudGateway>()
+        val accountManagementGateway = mockk<IAccountManagementGateway>()
+        val creditCardDAO = mockk<ICreditCardDAO>()
+        val creditCardService = CreditCardService(creditCardDAO, antiFraudGateway, accountManagementGateway)
+
+        val taxId = "12345678900"
+        val printedName = "Livio"
+        val creditCardCreation = CreditCardCreation(taxId, printedName)
+
+        val exception = assertThrows<BusinessRuleException> {
+            creditCardService.requestCreation(creditCardCreation)
+        }
+        assertEquals(BusinessRuleException("Provided CPF is invalid.").message, exception.message)
+
+    }
+
+    private fun getCreditCardWithOwnerAndPrintedName(taxID: String, printedName: String): CreditCard {
+        return CreditCard(
+            id = "",
+            owner = taxID,
+            number = "",
+            securityCode = "",
+            printedName = printedName,
+            creditLimit = 0.0,
+            availableCreditLimit = 0.0,
+        )
     }
 
     private fun getRandomCreditCard(): CreditCard {
